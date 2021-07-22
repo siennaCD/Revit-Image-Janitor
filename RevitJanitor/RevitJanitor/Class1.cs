072a96725d7ded0253535c7c0a655ac860d0829c
skbrown <skbrown@cannondesign.com>
2021-07-22 11:45:38 -0500

    addin works with revit

---
+++ RevitJanitor/RevitJanitor/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+namespace RevitJanitor
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class Class1 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Get application and document objects
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            TaskDialog.Show("Revit", "This is Sienna's Image Janitor Plugin");
+
+            return Result.Succeeded;
+        }
+    }
+}
