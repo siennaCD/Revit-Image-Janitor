813446c1c9b71c963c9f4a59148a744415d108cc
skbrown <skbrown@cannondesign.com>
2021-07-26 15:05:26 -0500

    can list all project materials

--- RevitJanitor/RevitJanitor/Class1.cs
+++ RevitJanitor/RevitJanitor/Class1.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Visual;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
 namespace RevitJanitor
@@ -23,7 +25,41 @@ namespace RevitJanitor
 
             TaskDialog.Show("Revit", "This is Sienna's Image Janitor Plugin");
 
+            ListMaterials(doc);
+
             return Result.Succeeded;
+        }
+
+        public void ListMaterials(Document doc)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            var materials = collector.WherePasses(new ElementClassFilter(typeof(Material))).Cast<Material>();
+
+            foreach (Material mat in materials)
+            {
+                Debug.Print($"{mat.Name}");
+            }
+        }
+
+        public void AlterAsset(Document doc, Material mat)
+        {
+            ElementId appearanceaseetId = mat.AppearanceAssetId;
+            AppearanceAssetElement assetElem =
+                mat.Document.GetElement(appearanceaseetId) as AppearanceAssetElement;
+
+            using (Transaction t = new Transaction(assetElem.Document, "Change material asset"))
+            {
+                t.Start();
+                using (AppearanceAssetEditScope editScope = new AppearanceAssetEditScope(doc))
+                {
+                    Asset editableAsset = editScope.Start(assetElem.Id);
+
+                    //AssetProperty bumpMapProperty = editableAsset["generic_bump_map"];
+
+                    editScope.Commit(true);
+                }
+                t.Commit();
+            }
         }
     }
 }
